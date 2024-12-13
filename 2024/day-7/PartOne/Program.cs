1b70928d06f621a25ce2f80c493b3ad39eb43ee8
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2024-12-12 21:01:25 -0300

    add day 7 part one

---
+++ 2024/day-7/PartOne/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Diagnostics;
+
+StreamReader reader = new("../input");
+
+List<(long Total, int[] parts)> calibrationEquations = new();
+for (string? line = reader.ReadLine(); line is not null; line = reader.ReadLine())
+{
+    string[] parts = line.Split(": ");
+    Debug.Assert(parts.Length == 2);
+    calibrationEquations.Add((long.Parse(parts[0]), parts[1].Split(" ").Select(int.Parse).ToArray()));
+}
+
+long result = 0;
+Operation[] operations = [Operation.Add, Operation.Multiply];
+
+foreach (var (total, parts) in calibrationEquations)
+{
+    if (Calculate(parts, operations, parts.Length - 1).Contains(total))
+        result += total;
+}
+
+Console.WriteLine(result);
+
+List<long> Calculate(int[] parts, Operation[] operations, int index)
+{
+    if (index == 0)
+        return [parts[index]];
+    var partials = Calculate(parts, operations, index - 1);
+    List<long> result = new(capacity: partials.Count * 2);
+    foreach (var partial in partials)
+    {
+        foreach (var operation in operations)
+        {
+            if (operation == Operation.Add)
+                result.Add(parts[index] + partial);
+            else if (operation == Operation.Multiply)
+                result.Add(parts[index] * partial);
+        }
+    }
+    return result;
+}
+
+enum Operation { Add, Multiply }
